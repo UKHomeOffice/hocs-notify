3a9c5a5b5be037a77b4c024c59381bb09b5e7898
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-03-17 14:50:12 +0000

    HOCS-2368: remove image_version param (#11)
    Currently we have two versions: `VERSION` and `IMAGE_VERSION`. To
    try and consolidate this we are defaulting to `VERSION`.

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,7 @@ set -euo pipefail
 export KUBE_NAMESPACE=${ENVIRONMENT}
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
-
-if [[ -z ${VERSION} ]] ; then
-    export VERSION=${IMAGE_VERSION}
-fi
+export VERSION=${VERSION}
 
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
@@ -16,11 +13,6 @@ then
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="2"
-fi
-
-if [[ -z ${KUBE_TOKEN} ]] ; then
-    echo "Failed to find a value for KUBE_TOKEN - exiting"
-    exit -1
 fi
 
 cd kd
