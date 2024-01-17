1547d9af2c909c6ded1b95db9f13d02800293a3b
vinothon84 <vinothon84@gmail.com>
2024-01-17 15:56:48 +0000

    HOCS-6855: Build changes to include latest spring-cloud-aws-messaging. Removed deprecated EnableSqs annotation. Added spring-messaging to resolve PayloadArgumentResolver class not found issue

--- src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
@@ -6,14 +6,12 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
-import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import io.awspring.cloud.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
-@EnableSqs
 @Configuration
 @Profile({"sqs"})
 public class SqsConfiguration {
