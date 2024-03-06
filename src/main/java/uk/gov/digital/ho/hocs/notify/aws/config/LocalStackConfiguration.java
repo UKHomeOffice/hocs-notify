7f53c2e92c05535937f6c9026215de36c1553c6a
vinothon84 <vinothon84@gmail.com>
2024-03-06 11:34:36 +0000

    Replaced SqsClient with SqsAsyncClient code

--- src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/LocalStackConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/LocalStackConfiguration.java
@@ -1,16 +1,14 @@
 package uk.gov.digital.ho.hocs.notify.aws.config;
 
-import io.awspring.cloud.sqs.config.SqsBootstrapConfiguration;
 import lombok.SneakyThrows;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
-import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
@@ -20,13 +18,27 @@ import static software.amazon.awssdk.regions.Region.EU_WEST_2;
 @Profile({"local"})
 public class LocalStackConfiguration {
 
-    @SneakyThrows
+   /* @SneakyThrows
     @Primary
     @Bean
     public SqsClient awsSqsClient(
             @Value("${aws.sqs.config.url}") String awsBaseUrl) {
 
         return  SqsClient.builder()
+                .region(EU_WEST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("test", "test")))
+                .endpointOverride(new URI(awsBaseUrl))
+                .build();
+    }*/
+
+    @SneakyThrows
+    @Primary
+    @Bean
+    public SqsAsyncClient awsSqsClient(
+            @Value("${aws.sqs.config.url}") String awsBaseUrl) {
+
+        return SqsAsyncClient.builder()
                 .region(EU_WEST_2)
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create("test", "test")))
