5e7ac4cd03a05896c1be0fb1cf14fd574a6246e3
vinothon84 <vinothon84@gmail.com>
2024-03-13 11:08:09 +0000

    Docker entrypoint JarLauncher package changes. Tidy up of unused packages in code

--- src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
@@ -1,24 +1,18 @@
 package uk.gov.digital.ho.hocs.notify.aws.config;
 
-import io.awspring.cloud.sqs.config.SqsBootstrapConfiguration;
-import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.context.annotation.*;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
-
-import java.net.URI;
 
 import static software.amazon.awssdk.regions.Region.EU_WEST_2;
 
 @Configuration
 @Profile({"sqs"})
 public class SqsConfiguration {
-    
+
     @Bean
     public SqsAsyncClient sqsAsyncClient(@Value("${aws.sqs.access.key}") String accessKey,
                                          @Value("${aws.sqs.secret.key}") String secretKey) {
