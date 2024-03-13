da0ace26f530229d224d0c2f582d59eee31b0b82
vinothon84 <vinothon84@gmail.com>
2024-03-13 15:50:00 +0000

    Add netty-nio-client and build custom HttpClientBuilder

--- src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/notify/aws/config/SqsConfiguration.java
@@ -7,6 +7,9 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+
+import java.time.Duration;
 
 @Configuration
 @Profile({"sqs"})
@@ -16,11 +19,16 @@ public class SqsConfiguration {
     public SqsAsyncClient sqsAsyncClient(@Value("${aws.sqs.access.key}") String accessKey,
                                          @Value("${aws.sqs.secret.key}") String secretKey,
                                          @Value("${aws.sqs.region}") Region region) {
+
+        NettyNioAsyncHttpClient.Builder httpClient = NettyNioAsyncHttpClient.builder()
+                .connectionAcquisitionTimeout(Duration.ofSeconds(20));
+
         return SqsAsyncClient.builder()
                 .region(region)
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(accessKey, secretKey)
                 ))
+                .httpClientBuilder(httpClient)
                 .build();
     }
 
